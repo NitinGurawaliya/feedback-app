afe2407bcfdc47a77364a501022f207a7a97529b
Your Name <221030201@juitsolan.in>
2026-04-03 13:21:42 +0530

    shadcn init

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Popover, PopoverContent, PopoverDescription, PopoverHeader, PopoverTitle, PopoverTrigger } from "@/components/ui/popover";
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
 
 export default function Home() {
   return (
     <div>
-      hello
+
     </div>
   );
 }
